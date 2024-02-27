a1d8e2a08a1bb5d416eca303960708106b174254
iridescent <2389504513@qq.com>
2024-02-27 16:08:49 +0800

    ✨ feat: 样式自适应

---
+++ core/styleSheet/config.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+import { Text, TextInput } from 'react-native'
+
+if (!Text.defaultProps) { Text.defaultProps = {} }
+if (!TextInput.defaultProps) { TextInput.defaultProps = {} }
+Text.defaultProps.allowFontScaling = false
+TextInput.defaultProps.allowFontScaling = false
